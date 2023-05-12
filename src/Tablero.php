2192f7c97dcb518201bff94e8a5af8758908b91d
Ariel Enferrel <you@example.com>
2023-05-12 20:04:45 -0300

    agregado metodo de carga del tablero y validacion de celda

--- src/Tablero.php
+++ src/Tablero.php
@@ -1,4 +1,4 @@
-<?php class tableroClases {
+<?php class Tablero {
  private $arregloCuadricula;
 
     public function __construct(){
@@ -42,6 +42,62 @@
             }
         }
       
+}
+
+public function llenarTablero($arreglo)
+{
+    $contadorFila = -1;
+    for ($filaTab = 0; $filaTab < 3; $filaTab++) {
+        for ($filaOb = 0; $filaOb < 3; $filaOb++) {
+            $contadorCol = 0;
+            $contadorFila++;
+            for ($colTab = 0; $colTab < 3; $colTab++) {
+                for ($colOb = 0; $colOb < 3; $colOb++) {
+                    $this->arregloCuadricula[$filaTab][$colTab]->getArregloCeldas()[$filaOb][$colOb]->setNum($arreglo[$contadorFila][$contadorCol]);
+                    $contadorCol++;
+                }
+            }
+        }
+    }
+}
+
+public function esValido($filTab, $colTab, $filCuad, $colCuad, $numero){
+    // Verificar si el número ya está en la fila
+    for ($columnaTab = 0; $columnaTab < 3; $columnaTab++) {
+        
+        for ($columnaCuad = 0; $columnaCuad < 3; $columnaCuad++) {
+              if ($this->arregloCuadricula[$filTab][$columnaTab]->getArregloCeldas()[$filCuad][$columnaCuad]->getNum() == $numero) {
+                return false;
+            }
+        }
+    }
+
+    // Verificar si el número ya está en la columna
+
+    for ($filaTab = 0; $filaTab < 3; $filaTab++) {
+      
+        for ($filaCuad = 0; $filaCuad < 3; $filaCuad++) {
+           
+            if ($this->arregloCuadricula[$filaTab][$colTab]->getArregloCeldas()[$filaCuad][$colCuad]->getNum() == $numero) {
+                return false;
+            }
+        }
+    }
+
+    // Verificar si el número ya está en la submatriz
+
+    for ($filaCuad = 0; $filaCuad < 3; $filaCuad++) {
+       
+        for ($columnaCuad = 0; $columnaCuad < 3; $columnaCuad++) {
+            
+            if ($this->arregloCuadricula[$filTab][$colTab]->getArregloCeldas()[$filaCuad][$columnaCuad]->getNum() == $numero) {
+                return false;
+            }
+        }
+    }
+
+    // Si no se ha encontrado ninguna coincidencia, entonces el número es válido
+    return true;
 }
 
 }
